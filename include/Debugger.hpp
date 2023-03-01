14a9b842c5c89d652658bdc52995b6bb1a3b4567
Toby <tobyeverist1@gmail.com>
2023-03-01 17:39:34 +0000

    Add breakpoint support

--- include/Debugger.hpp
+++ include/Debugger.hpp
@@ -16,12 +16,17 @@ public:
     Debugger (std::string prog_name, pid_t pid)
         : m_prog_name{std::move(prog_name)}, m_pid{pid} {}
 
+
+    void set_breakpoint_at_address(std::intptr_t addr);
+
     void run();
 
 private:
     std::string m_prog_name;
     pid_t m_pid;
+    std::unordered_map<std::intptr_t, std::unique_ptr<Breakpoint>> m_breakpoints;
 };
 
 
 #endif // DEBUGGER_HPP
+
--- src/Debugger.cpp
+++ src/Debugger.cpp
@@ -19,8 +19,24 @@ void Debugger::handle_command(const std::string& line) {
     auto args = split(line,' ');
     auto command = args[0];
 
-    if (is_prefix(command, "continue")) {
+    if (is_prefix(command, "cont")) {
         continue_execution();
+    }
+    else if(is_prefix(command, "break")) {
+        // Check that the user has given us an argument
+        if (args.size() != 2) {
+            std::cerr << "Please specify an address\n";
+            return;
+        }
+        // Check that the argument is a valid hex number
+        auto result = strtoul(args[1].c_str(), nullptr, 16);
+        if (result == 0) {
+            std::cerr << "Please specify a valid hex number\n";
+            return;
+        }        
+
+        std::string addr {args[1], 2}; //naively assume that the user has written 0xADDRESS
+        set_breakpoint_at_address(std::stol(addr, 0, 16));
     }
     else {
         std::cerr << "Unknown command\n";
@@ -53,4 +69,11 @@ std::vector<std::string> split(const std::string &s, char delimiter) {
 bool is_prefix(const std::string& s, const std::string& of) {
     if (s.size() > of.size()) return false;
     return std::equal(s.begin(), s.end(), of.begin());
+}
+
+void Debugger::set_breakpoint_at_address(std::intptr_t addr) {
+    std::cout << "Set breakpoint at address " << std::hex << addr << std::endl;
+    auto breakpoint = std::make_unique<Breakpoint>(m_pid, addr);
+    breakpoint->enable();
+    m_breakpoints.insert({addr, std::move(breakpoint)});
 }
