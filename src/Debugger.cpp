950deaf865aca681527d2d25282e6c09ea6aee61
Toby <tobyeverist1@gmail.com>
2023-03-02 21:23:56 +0000

    Add support for registers

--- src/Debugger.cpp
+++ src/Debugger.cpp
@@ -38,6 +38,29 @@ void Debugger::handle_command(const std::string& line) {
         std::string addr {args[1], 2}; //naively assume that the user has written 0xADDRESS
         set_breakpoint_at_address(std::stol(addr, 0, 16));
     }
+    else if (is_prefix(command, "register")) {
+        if (is_prefix(args[1], "dump")) {
+            dump_registers();
+        }
+        else if (is_prefix(args[1], "read")) {
+            std::cout << get_register_value(m_pid, get_register_from_name(args[2])) << std::endl;
+        }
+        else if (is_prefix(args[1], "write")) {
+            std::string val {args[3], 2}; //assume 0xVAL
+            set_register_value(m_pid, get_register_from_name(args[2]), std::stol(val, 0, 16));
+        }
+    }
+    else if(is_prefix(command, "memory")) {
+        std::string addr {args[2], 2}; //assume 0xADDRESS
+
+        if (is_prefix(args[1], "read")) {
+            std::cout << std::hex << read_memory(std::stol(addr, 0, 16)) << std::endl;
+        }
+        if (is_prefix(args[1], "write")) {
+            std::string val {args[3], 2}; //assume 0xVAL
+            write_memory(std::stol(addr, 0, 16), std::stol(val, 0, 16));
+        }
+    }
     else {
         std::cerr << "Unknown command\n";
     }
@@ -45,11 +68,9 @@ void Debugger::handle_command(const std::string& line) {
 
 
 void debugger::continue_execution() {
+    step_over_breakpoint();
     ptrace(PTRACE_CONT, m_pid, nullptr, nullptr);
-
-    int wait_status;
-    auto options = 0;
-    waitpid(m_pid, &wait_status, options);
+    wait_for_signal();
 }
 
 
@@ -77,3 +98,60 @@ void Debugger::set_breakpoint_at_address(std::intptr_t addr) {
     breakpoint->enable();
     m_breakpoints.insert({addr, std::move(breakpoint)});
 }
+
+
+// TODO: make an I/O manipulator to get rid of this mess.
+void Debugger::dump_registers() {
+    for (const auto& rd : g_register_descriptors) {
+        std::cout << rd.name << " 0x"
+                  << std::setfill('0') << std::setw(16) << std::hex << get_register_value(m_pid, rd.r) << std::endl;
+    }
+}
+
+
+uint64_t Debugger::read_memory(uint64_t address) {
+    m_iov.iov_base = reinterpret_cast<void*>(address);
+    m_iov.iov_len = sizeof(uint64_t);
+    return process_vm_readv(m_pid, &m_iov, 1, &m_iov, 1, 0);
+}
+
+void Debugger::write_memory(uint64_t address, uint64_t value) {
+    m_iov.iov_base = reinterpret_cast<void*>(address);
+    m_iov.iov_len = sizeof(value);
+    m_iov.iov_base = &value;
+    process_vm_writev(m_pid, &m_iov, 1, &m_iov, 1, 0);
+}
+
+
+uint64_t Debugger::get_pc() {
+    return get_register_value(m_pid, Register::rip);
+}
+
+void Debugger::set_pc(uint64_t pc) {
+    set_register_value(m_pid, Register::rip, pc);
+}
+
+void Debugger::step_over_breakpoint() {
+    auto possible_breakpoint_location = get_pc() - 1; // -1 because we want to be on the breakpoint instruction
+    if (m_breakpoints.count(possible_breakpoint_location)) {
+        auto& bp = m_breakpoints[possible_breakpoint_location];
+
+        if (bp.is_enabled()) {
+            auto previous_instruction_address = possible_breakpoint_location;
+            set_pc(previous_instruction_address);
+
+            bp.disable();
+            ptrace(PTRACE_SINGLESTEP, m_pid, nullptr, nullptr);
+            wait_for_signal();
+            bp.enable();
+        }
+    }
+}
+
+
+void Debugger::wait_for_signal() {
+    int wait_status;
+    auto options = 0;
+    waitpid(m_pid, &wait_status, options);
+}
+
